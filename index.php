b9002101a46df1125004e7f22efdc83d9d0dbc85
Lacongnam <namchi2810@gmail.com>
2023-06-12 21:27:43 +0700

    upcode

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+//http://localhost/Project3/ctrangchu/
+session_start();
+require_once "./mvc/bridge.php";
+$myApp = new App();
+?>
---
+++ mvc/models/mphanloai.php
@@ -0,0 +1,11 @@
+<?php
+require_once "database.php";
+class mphanloai extends database
+{
+    public function getPhanloai()
+    {
+        $query = "select * from phanloai";
+        return $this->select($query);
+    }
+}
+?>
